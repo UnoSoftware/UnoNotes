cb5bc276ae67a987bb7461dd0bc4c3624de9274d
Santiago Arias <santi.ari.pani@outlook.com>
2022-10-13 14:48:15 +0200

    Inicio de sesión

--- app/src/main/java/com/astra/notes/SignInActivity.kt
+++ app/src/main/java/com/astra/notes/SignInActivity.kt
@@ -6,7 +6,9 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -14,16 +16,30 @@ import kotlin.math.sign
 
 class SignInActivity : AppCompatActivity() {
 
-    var email: EditText = findViewById(R.id.Email)
-    private var password: EditText = findViewById(R.id.password)
-    var loginbtn: Button = findViewById(R.id.loginbtn)
-    var signupredirect: TextView = findViewById(R.id.noaccount)
-    lateinit var auth: FirebaseAuth
+    private lateinit var email: EditText
+    private lateinit var password: EditText
+    private lateinit var loginbtn: Button
+    private lateinit var signupredirect: TextView
+    private lateinit var auth: FirebaseAuth
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+        email = findViewById(R.id.Email)
+        password = findViewById(R.id.password)
+        loginbtn = findViewById(R.id.loginbtn)
+        signupredirect = findViewById(R.id.noaccount)
         auth = Firebase.auth
+
+        loginbtn.setOnClickListener {
+            auth.signInWithEmailAndPassword(email.text.toString(), password.text.toString()).addOnCompleteListener {
+                if (it.isSuccessful) load()
+                else {
+                    Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
 
         //Actualizar cuando esté la página de registro
         signupredirect.setOnClickListener {
@@ -37,11 +53,12 @@ class SignInActivity : AppCompatActivity() {
         super.onStart()
         val currentUser = auth.currentUser
         if (currentUser != null) {
-            reload();
+            load();
         }
     }
 
-    private fun reload() {
+    private fun load() {
+        Toast.makeText(this, "Authentication successful", Toast.LENGTH_SHORT).show()
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
     }
