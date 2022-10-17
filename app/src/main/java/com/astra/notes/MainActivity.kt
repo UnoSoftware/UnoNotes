be071a4c4d040f9e699b1bee764b5617e0f05039
nachocaniculo <102062992+nachocaniculo@users.noreply.github.com>
2022-10-17 12:50:48 +0200

    menu

--- app/src/main/java/com/astra/notes/MainActivity.kt
+++ app/src/main/java/com/astra/notes/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.CheckBox
 import android.widget.ImageButton
@@ -12,6 +14,7 @@ import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import org.w3c.dom.Text
 import kotlinx.android.synthetic.main.activity_main.*
@@ -20,6 +23,8 @@ import kotlinx.android.synthetic.main.activity_main.view.*
 class MainActivity : AppCompatActivity() {
 
     private val db = FirebaseFirestore.getInstance()
+
+    private val auth = FirebaseAuth.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -57,5 +62,23 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.logout ->{
+                auth.signOut()
+                val intent = Intent(this, SignInActivity::class.java)
+                startActivity(intent)
+            }
+            R.id.settings ->{
+                val intent = Intent(this, SettingsActivity::class.java)
+                startActivity(intent)
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
